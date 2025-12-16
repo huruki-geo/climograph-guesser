548ac92eb64c10718800a6fd65df2b8e1fc70880
huruki-geo <lot.ofkzmnpc@gmail.com>
2025-12-16 12:34:58 +0900

    ssrok?

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,22 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+          output: {
+            manualChunks: {
+              'react-vendor': ['react', 'react-dom'],
+              'chart-vendor': ['recharts', 'd3-scale', 'd3-shape'],
+              'map-vendor': ['leaflet'],
+            }
+          }
+        }
+      },
+      ssr: {
+        noExternal: ['react', 'react-dom']
       }
     };
 });
